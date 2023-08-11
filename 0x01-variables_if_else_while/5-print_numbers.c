ac108313fcd502b55e8f9018d146070e91439208
jewharoali <jewharoali@gmail.com>
2023-08-11 13:46:02 -0700

    prints singlr digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
