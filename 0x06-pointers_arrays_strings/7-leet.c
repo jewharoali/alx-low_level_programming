7d259cea9949acada0ceba16681c910b0bdd08e1
jewharoali <jewharoali@gmail.com>
2023-08-24 17:24:46 -0700

    encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,12 +13,11 @@ char *leet(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; replacement[j] != '\0'; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (str[i] == replacement[j])
 			{
 				str[i] = leetChar[j];
-				break;
 			}
 		}
 	}
