b2ddc9baf98a9d0400b12e8920e81f39c7183ce7
jewharoali <jewharoali@gmail.com>
2023-08-30 14:14:31 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - that prints a string
+ * @s:the string being printed
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
