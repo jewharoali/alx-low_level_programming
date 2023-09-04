e2f363ee09096efa1a27d5ceada80cbc37b49819
jewharoali <jewharoali@gmail.com>
2023-09-04 14:13:10 -0700

    counts the number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @argc:argument count
+ * @argv:argument list
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
