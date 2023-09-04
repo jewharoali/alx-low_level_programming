6eee29e6cc8d970e0cc2a3440dbf11eefd1b80b2
jewharoali <jewharoali@gmail.com>
2023-09-04 14:07:06 -0700

    prints the name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @argc:argument count
+ * @argv:argument list
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
