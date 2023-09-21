495f77e8c1137f229cebb9f3e2320b501326545e
jewharoali <jewharoali@gmail.com>
2023-09-21 14:08:15 -0700

    list length

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LIST_H
+#define LISTS_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -22,5 +22,6 @@ typedef struct list_s
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
