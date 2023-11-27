b2e4b0227cbf74d7f95b6cf0e3a264d8bcea4f26
jewharo ali <jewharoali@gmail.com>
2023-11-27 06:42:18 -0800

    prints all char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ * @str:string being printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
