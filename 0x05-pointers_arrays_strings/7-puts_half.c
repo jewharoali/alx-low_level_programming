74a65d3218ab877535d63daa65fcc640b5bf6fe1
jewharo ali <jewharoali@gmail.com>
2024-01-09 08:02:51 -0800

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,18 @@
  */
 void puts_half(char *str)
 {
-	int i = 0, length = 0, mid;
+	int i = 0, length = 0, mid, n;
 
 	while (str[length] != '\0')
 		length++;
 	mid = length / 2;
+	n = (length - 1) / 2;
 	if (length % 2 == 0)
 	{
 		for (i = mid; i < length; i++)
 			_putchar(str[i]);
 	}
 	else
-	{
-		for (i = mid - 1; i < length; i++)
-			_putchar(str[i]);
-	}
+		_putchar(n);
 	_putchar('\n');
 }
