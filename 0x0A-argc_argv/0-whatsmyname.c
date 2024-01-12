4d95088451c560d611efb8bf497d52dfa3d2ec25
jewharoali <jewharoali@gmail.com>
2024-01-12 11:55:42 -0800

    name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc:argument count
+ * @argv:character array
+ * Return:0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
