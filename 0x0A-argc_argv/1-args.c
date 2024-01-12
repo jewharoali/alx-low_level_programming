6122968506be72512bf247f79360cfbf62916072
jewharoali <jewharoali@gmail.com>
2024-01-12 12:26:14 -0800

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc:argument count
+ * @argv:character array
+ * Return:0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
