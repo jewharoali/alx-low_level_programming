cb9fd3c1e1701c9aa50424d383de055e77e8ad58
jewharoali <jewharoali@gmail.com>
2024-03-12 03:05:35 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,6 +5,7 @@
  * @s:pointer to the space in memory to be filled
  * @b:value to be filled
  * @n:number of bytes to filled
+ * Return:ptr to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
