3e0e04636567b093003ee735d94e8903f39a5b1b
jewharoali <jewharoali@gmail.com>
2024-03-12 03:01:51 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant bytee
+ * @s:pointer to the space in memory to be filled
+ * @b:value to be filled
+ * @n:number of bytes to filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr;
+
+	ptr = s + n;
+	s[n] = b;
+	return (ptr);
+}
