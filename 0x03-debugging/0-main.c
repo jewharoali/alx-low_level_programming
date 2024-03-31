d3c3ecc888f0e586cd145c48282e8f377a7b5b18
jewharoali <jewharoali@gmail.com>
2024-03-31 07:17:15 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - tests for whether a number is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
