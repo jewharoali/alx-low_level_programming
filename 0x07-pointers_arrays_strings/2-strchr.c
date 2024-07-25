39c9f3e6e7b8f25a462187dc97d4947607ca317b
jewharoali <jewharoali@gmail.com>
2024-07-25 03:32:38 -0700

    first commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strchr - a function that locates a character in a string.
+ * @s:the string
+ * @c:the character being looked for
+ * Return:the character or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *ptr;
+	
+	ptr = NULL;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			break;
+		}
+	}
+	return (ptr);
+}
