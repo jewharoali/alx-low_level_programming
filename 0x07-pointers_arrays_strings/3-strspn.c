89891cfc958329b7b378d41257df43ff4f3fae3d
jewharoali <jewharoali@gmail.com>
2024-07-27 03:10:59 -0700

    a fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,15 +8,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
+	int i, j;
 	unsigned int ptr = 0;
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (s[i] == accept[i])
+		for (j = 0; accept[j] >= '\0'; j++)
 		{
-			ptr++;
+			if (s[i] == accept[j])
+			{
+				ptr++;
+				break;
+			}
 		}
+		if (j == '\0')
+			break;
 	}
 	return (ptr);
 }
