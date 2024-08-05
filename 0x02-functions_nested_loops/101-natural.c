a6a6c823013cf574b5a453ee5b464b88efef6d9e
jewharoali <jewharoali@gmail.com>
2024-08-05 01:19:20 -0700

    multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int i, n, sum;
+
+	n = 1024;
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
