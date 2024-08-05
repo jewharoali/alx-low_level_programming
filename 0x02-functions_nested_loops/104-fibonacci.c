a7b6ed51109a1173ea3f1748998f183668a68ecb
jewharoali <jewharoali@gmail.com>
2024-08-05 03:51:49 -0700

    fibonacci2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: return 0 when successful
+ */
+int main(void)
+{
+	unsigned long int i, n, t1, t2, nextTerm;
+
+	t1 = 1, t2 = 2, n = 98;
+
+	printf("%lu", t1);
+	printf(", %lu", t2);
+
+	for (i = 3; i <= 98; i++)
+	{
+		nextTerm = t1 + t2;
+		printf(", %lu", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+	}
+	printf("\n");
+	return (0);
+}
